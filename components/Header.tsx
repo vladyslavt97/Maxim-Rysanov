149ebeaf45df516827b201ab7f36c01ddbee5288
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-25 21:28:27 +0100

    Menu and header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 import { useState } from 'react';
-import { log } from 'console';
+import { motion } from 'framer-motion';
 
 type Props = {}
 
@@ -13,27 +13,63 @@ export default function Header({}: Props) {
     }
 
     return (
-        <header className='sticky top-0 p-5 flex flex-row justify-between bg-slate-500 items-center'>
-            {/* items-start max-w-7xl mx-auto z-20 xl:items-center */}
+        <header className='sticky top-0 p-1 flex flex-row justify-between bg-slate-500 items-center'>
             <Image src="/logo.png" alt='logo' width={300} height={300}/>
-            <div className='space-x-3 hidden md:flex font-semibold text-xl text-gray-300'>
+            <motion.div 
+                animate={{
+                    opacity: 0
+                }}
+                transition={{
+                    duration: 1
+                }}
+                whileInView={{
+                    opacity: 1
+                }}
+                className={menu ? "absolute right-[100px] top-[100px] flex flex-col z-20 justify-end items-end md:hidden" 
+                : 'space-x-3 hidden md:flex font-semibold text-xl text-gray-300'}>
             <Link href="#bio">
-                <button className=''>Biography</button>
+                <button className={menu ? 'py-5': ''}>Biography</button>
             </Link>
             <Link href="#concerts">
-                <button className=''>Concerts</button>
+                <button className={menu ? 'py-5': ''}>Concerts</button>
             </Link>
             <Link href="#media">
-                <button className=''>Media</button>
+                <button className={menu ? 'py-5': ''}>Media</button>
             </Link>
             <Link href="#contacts">
-                <button className=''>Contacts</button>
+                <button className={menu ? 'py-5': ''}>Contacts</button>
             </Link>
+            </motion.div>
+
+
+            <motion.div className='absolute rounded-full bg-yellow-200 w-[50px] h-[50px] right-[23px] md:hidden'
+                animate={{
+                    // scale: menu ? 4.2 : 0.2,
+                    y: menu ? 250 : 0,
+                    x: menu ? 25 : 0,
+                    width: menu ? '50%' : '50px',
+                    height: menu ? '100vh' : '50px',
+                    borderRadius: menu ? '0%' : "100%"
+                }}
+                transition={{ duration: 0.3 }}>
+                </motion.div>
+                
+            <div onClick={e => toggleMenu()} className="z-10 md:hidden">
+                
+                <motion.div 
+                animate={{rotate: menu ? -45 : 0}}
+                className='h-[2px] w-[40px] bg-black m-2'></motion.div>
+                <motion.div 
+                animate={{opacity: menu ? 0 : 1}}
+                transition={{ duration: 0.1 }}
+                className='h-[2px] w-[40px] bg-black m-2'></motion.div>
+                <motion.div 
+                animate={{
+                    rotate: menu ? 45 : 0,
+                    y: menu ? -19.5 : 0,
+                }}
+                className='h-[2px] w-[40px] bg-black m-2'></motion.div>
             </div>
-            <Image src="/menu.png"  alt='menu' width={100} height={100} 
-            className="md:hidden cursor-pointer"
-            onClick={e => toggleMenu()}
-            />
         </header>
   )
 }
