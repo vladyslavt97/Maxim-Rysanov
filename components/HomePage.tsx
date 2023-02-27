103e40ee3303f65b00bfe4e1eb40272b6da87a92
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-02-27 14:43:10 +0100

    Home page - looks amazing

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -5,15 +5,15 @@ type Props = {}
 
 export default function HomePage({}: Props) {
   return (
-    <div className='absolute top-[70px] bg-white w-[90vw] h-[85vh] m-[2.5vh]'>
-        <div className='absolute w-[45%] top-[10%] sm:w-[50%] p-5 flex justify-center items-start h-[70vh] text-gray-800 flex-col text-sm md:text-md z-30 md:text-2xl md:ml-[12%]'>
-            <p className='italic'>
+    <div className='absolute top-[70px] bg-white w-[90vw] h-[85vh] m-[2.5vh] rounded'>
+        <div className='absolute top-[10%] flex justify-center items-start h-[70vh] text-gray-800 flex-col text-sm md:text-md z-30 md:text-2xl md:ml-[12%]'>
+            <p className='italic w-[45%] sm:w-[50%] p-5'>
                 &quot;The colours and contrasts that Rysanov manages to extract from the orchestra are truly fabulous!&quot; 
                 <br/>
                 &quot;We already know some very beautiful versions of this symphony by Vasks, including one by Kremerata Baltica on Teldec; but Rysanov and the Riga orchestra raise it to the top of the discography!&quot;
             </p>
             <br/>
-            <p className='text-gray-500'>Crescendo Magazine, Olivier Vrins, 18 May 2020</p>
+            <p className='text-gray-500 w-full p-5 text-xs'>Crescendo Magazine, Olivier Vrins, 18 May 2020</p>
         </div>
         <motion.div
         initial={{
