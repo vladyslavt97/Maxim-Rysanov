cdeaf856e62b206677c4821f1ea8a56d26a060d0
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-03-12 18:08:11 +0100

    before adding the switch

---
+++ components/LangiagesSwitch.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function LangiagesSwitch({}: Props) {
+  return (
+    <div>LangiagesSwitch</div>
+  )
+}
--- pages/bio.tsx
+++ pages/bio.tsx
@@ -1,7 +1,7 @@
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
-
+import LanguagesSwitch from '@/components/LangiagesSwitch'
 type Props = {}
 
 export default function bio({}: Props) {
@@ -9,6 +9,7 @@ export default function bio({}: Props) {
     <div className='h-full bg-gray-400'>
         <Header />
         <div className='absolute top-[82px] flex flex-col justify-center items-center mx-2 p-5 pt-8 pb-40 md:pb-10 text-black bg-gradient-to-tr from-neutral-100 to-sky-200 rounded'>
+            <LanguagesSwitch />
             <div className='flex justify-center items-center'>
               <h1 className='font-bold text-xl m-5 text-black'>Biography</h1>
             </div>
