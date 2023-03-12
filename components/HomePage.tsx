85be22a2f93821274537e2bb512673e5042f1894
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-03-12 20:18:04 +0100

    Homepage top-changes

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -6,7 +6,7 @@ type Props = {}
 export default function HomePage({}: Props) {
   return (
     <div className='bg-gradient-to-tr from-neutral-100 to-sky-200 w-full border-[10px] border-blue-300 absolute top-[70px] rounded text-black sm:h-[93%] h-[90%]'>
-        <div className='absolute top-[10%] flex justify-center items-start h-[60vh] text-gray-800 flex-col text-sm md:text-md z-30 md:text-2xl md:ml-[12%]'>
+        <div className='absolute top-[7%] flex justify-center items-start h-[60vh] text-gray-800 flex-col text-sm md:text-md z-30 md:text-2xl md:ml-[12%]'>
             <p className='italic w-[45%] sm:w-[50%] p-5'>
                 &quot;The colours and contrasts that Rysanov manages to extract from the orchestra are truly fabulous!&quot; 
                 <br/>
@@ -25,9 +25,8 @@ export default function HomePage({}: Props) {
         transition={{
             duration: 1
         }}
-        className="p-2"
         >
-            <Image src='/home.png' alt="image" width={180} height={180} className='absolute right-0 object-fit w-[200px] md:w-[300px] top-[10%]'/>
+            <Image src='/home.png' alt="image" width={180} height={180} className='absolute right-0 object-fit w-[200px] md:w-[300px] top-[6%]'/>
         </motion.div>
     </div>
   )
