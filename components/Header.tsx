a588b5afa36b81bd834482bb651f523054ccda75
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-03-16 09:36:27 +0100

    refactoring the sidebar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,15 +2,34 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useState } from 'react';
 import { motion } from 'framer-motion';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import Button from '@mui/material/Button';
 
 type Props = {}
+type Anchor = 'right';
 
 export default function Header({}: Props) {
     const [menu, setMenu] = useState(false);
+    const [state, setState] = useState({right: false});
 
     const toggleMenu = () => {
         setMenu(!menu)
     }
+
+    const toggleDrawer =
+    (anchor: Anchor, open: boolean) =>
+    (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setState({ ...state, [anchor]: open });
+    };
 
     return (
         <header className='fixed w-full top-0 p-1 pr-5 flex flex-row justify-between bg-gradient-to-r from-gray-700 to-gray-900 items-center z-50 h-[71px]'>
@@ -32,6 +51,7 @@ export default function Header({}: Props) {
                 </Link>
             </motion.div>
 
+                {/* Links */}
             <motion.div 
                 animate={{
                     opacity: 0
@@ -62,7 +82,7 @@ export default function Header({}: Props) {
             </Link>
             </motion.div>
 
-
+                {/* sidebar with framer */}
             <motion.div className={menu ? 'absolute rounded-full bg-gradient-to-tr from-gray-200 to-gray-300 w-[50px] h-[50px] right-[0px] top-0 md:hidden' : 'absolute rounded-full bg-gray-600/0 w-[50px] h-[50px] right-[23px] md:hidden'}
                 animate={{
                     width: menu ? '50%' : '50px',
@@ -73,8 +93,9 @@ export default function Header({}: Props) {
                 transition={{ duration: 0.5 }}>
                 </motion.div>
                 
+
+                {/* burger */}
             <div onClick={e => toggleMenu()} className="z-10 md:hidden">
-                
                 <motion.div 
                 animate={{rotate: menu ? -45 : 0}}
                  className={menu ? 'h-[2px] w-[40px] bg-gray-900 m-2' : 'h-[2px] w-[40px] bg-gray-300 m-2'}></motion.div>
@@ -89,6 +110,19 @@ export default function Header({}: Props) {
                 }}
                  className={menu ? 'h-[2px] w-[40px] bg-gray-900 m-2' : 'h-[2px] w-[40px] bg-gray-300 m-2'}></motion.div>
             </div>
+
+
+                {/* new
+                <Button onClick={toggleDrawer('right', true)}>right</Button>
+            <SwipeableDrawer
+                anchor={"right"}
+                open={state["right"]}
+                onClose={toggleDrawer("right", false)}
+                onOpen={toggleDrawer("right", true)}
+                className="z-20 text-black absolute top-[50vh] right-0"
+                >
+                Right sdfsafdsafs
+            </SwipeableDrawer> */}
         </header>
   )
 }
