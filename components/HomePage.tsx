6b2ed7f19c7c80ada2573c218f2f7b0b68357227
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-03-25 18:26:01 +0100

    Homepage and image

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export default function HomePage({}: Props) {
   return (
     <div className='bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px] rounded text-black sm:h-[93%] h-[90%]'>
-        <div className='absolute top-[6%] flex justify-center items-start h-[60vh] text-gray-800 flex-col text-sm md:text-md z-30 md:text-2xl md:ml-[12%]'>
+        <div className='absolute top-[6%] flex justify-center items-start h-[60vh] text-gray-800 flex-col text-sm md:text-md z-30 md:text-2xl xl:text-3xl xl:font-light lg:leading-relaxed md:ml-[5%] lg:ml-[12%]'>
             <p className='italic w-[45%] sm:w-[50%] p-5'>
                 &quot;The colours and contrasts that Rysanov manages to extract from the orchestra are truly fabulous!&quot; 
                 <br/>
@@ -24,7 +24,7 @@ export default function HomePage({}: Props) {
             opacity: 1
         }}
         transition={{
-            duration: 1
+            duration: 2
         }}
         id="home"
         >
@@ -32,7 +32,7 @@ export default function HomePage({}: Props) {
             {/* // animate={{ scale: [1, 1.1, 1] }}
             // transition={{duration: 10, repeat: Infinity,}} */}
             {/* > */}
-            <Image src='/home.png' alt="image" width={180} height={180} className='absolute right-0 object-fit w-[200px] md:w-[300px] top-[5%]'/>
+            <Image src='/home.png' alt="image" width={1000} height={1000} className='absolute right-0 object-fit w-[200px] sm:w-[400px] top-[5%] sm:top-[15%] xl:mr-36'/>
             {/* </motion.div> */}
         </motion.div>
     </div>
