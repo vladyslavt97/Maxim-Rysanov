f0d4e586f207952947a11018d9a276e0d3c9c41b
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-03-29 16:18:28 +0200

    Footer icons

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,10 +5,10 @@ type Props = {}
 
 export default function Footer({}: Props) {
   return (
-    <div className='fixed bottom-0 w-full bg-gray-800 text-white flex justify-center items-center border-t-[10px] border-r-[10px] border-l-[10px] border-gray-300 h-[71px]'>
-        <SocialIcon url="https://www.facebook.com/maximrysanov" bgColor='white' className='m-1 cursor-pointer'/>
-        <SocialIcon url="https://www.youtube.com/@MaximRysanov" bgColor='white' className='m-1 cursor-pointer'/>
-        <SocialIcon url="https://www.instagram.com/mrysanov_viola_conductor/"  bgColor='white' className='m-1 cursor-pointer'/>
+    <div className='fixed bottom-0 w-full bg-gray-800 text-white flex justify-evenly items-center border-t-[10px] border-r-[10px] border-l-[10px] border-gray-300 h-[51px]'>
+        <SocialIcon url="https://www.facebook.com/maximrysanov" bgColor='white' style={{ height: 35, width: 35 }} className='m-1 cursor-pointer'/>
+        <SocialIcon url="https://www.youtube.com/@MaximRysanov" bgColor='white' style={{ height: 35, width: 35 }} className='m-1 cursor-pointer'/>
+        <SocialIcon url="https://www.instagram.com/mrysanov_viola_conductor/"  bgColor='white' style={{ height: 35, width: 35 }} className='m-1 cursor-pointer'/>
     </div>
   )
 }
