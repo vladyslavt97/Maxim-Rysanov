5579d257cf23d476895c3a2e6a627b9b0badb89b
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-07 07:56:14 +0200

    2020 concerts

--- components/PastConcerts.tsx
+++ components/PastConcerts.tsx
@@ -6,19 +6,17 @@ type Props = {}
 
 export default function PastConcerts({}: Props) {
   const [open, setOpen]=useState(false);
-  const router = useRouter()
-
-  console.log(router.pathname)
-  
+  const router = useRouter();
   return (
     <div className='rounded--lg absolute right-0'>
-      <button className='py-2 pl-3 pr-1 bg-slate-500 rounded-lg flex flex-row text-center justify-center items-center text-white text-sm' onClick={e=>setOpen(!open)}>Past concerts<MdKeyboardArrowDown className='ml-1 text-white'/></button>
+      <button className='py-2 pl-3 pr-1 bg-slate-500 rounded-lg flex flex-row text-center justify-center items-center text-white text-sm drop-shadow-lg' onClick={e=>setOpen(!open)}>Past concerts<MdKeyboardArrowDown className='ml-1 text-white'/></button>
       {open && <div className='flex flex-col bg-white text-black rounded-lg'>
         {router.pathname === "/concerts-2023" && <Link href="/concerts" className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300 text-sm text-center'><button onClick={e=>setOpen(!open)}>Upcoming 2023</button></Link>}
         {router.pathname !== "/concerts-2023" && <Link href="/concerts-2023" className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300 text-sm text-center'><button onClick={e=>setOpen(!open)}>Concerts 2023</button></Link>}
         <Link href="/concerts-2022" className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300 text-sm text-center'><button onClick={e=>setOpen(!open)}>Concerts 2022</button></Link>
-        <Link href="/concerts-2021" className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300 text-sm text-center rounded-b-lg'><button onClick={e=>setOpen(!open)}>Concerts 2021</button></Link>
-        {/*<button className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300' onClick={e=>setOpen(!open)}><Link href="/concerts-2020">Concerts 2020</Link></button> */}
+        <Link href="/concerts-2021" className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300 text-sm text-center'><button onClick={e=>setOpen(!open)}>Concerts 2021</button></Link>
+        <Link href="/concerts-2020" className='hover:bg-slate-300 py-2 hover:rounded-lg border-b-2 border-gray-300 text-sm text-center rounded-b-lg'><button onClick={e=>setOpen(!open)}>Concerts 2020</button></Link>
+       
       </div>}
     </div>
   )
---
+++ pages/concerts-2020.tsx
@@ -0,0 +1,52 @@
+import PastConcerts from '@/components/PastConcerts';
+import concerts2020 from '@/concerts2020.json'
+type Props = {}
+interface ConcertType {
+  year: number | string;
+  date: string;
+  viola: string;
+  conductor: string;
+  location: string;
+  programme: string[];
+}
+export default function Concerts2022({}: Props) {
+    const validConcerts2020 = concerts2020.sort((a: ConcertType, b: ConcertType) => {
+      const [dayA, dateA] = a.date.split(' ')
+      const [ddA, mmA] = dateA.split('/')
+      const yyyyA = new Date().getFullYear()
+      const fullDateStringA:any = `${yyyyA}-${mmA}-${ddA}`
+      const dateObjectA = new Date(fullDateStringA)
+
+      const [dayB, dateB] = b.date.split(' ')
+      const [ddB, mmB] = dateB.split('/')
+      const yyyyB = new Date().getFullYear()
+      const fullDateStringB = `${yyyyB}-${mmB}-${ddB}`
+      const dateObjectB = new Date(fullDateStringB)
+
+      return dateObjectA.getTime() - dateObjectB.getTime()
+    })
+  return (
+    <div className='bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px] rounded text-black min-h-full flex flex-col items-center'>
+        <PastConcerts/>
+        <div className='flex justify-center items-center pt-5'>
+          <h1 className='font-bold text-xl'>2020</h1>
+        </div>
+        <div className="flex flex-col mx-3 mt-4 justify-center items-start mb-40 lg:text-xl">
+            {validConcerts2020.map((concert: ConcertType, index: number) => (
+                <div key={index} className="m-3">
+                <div className='flex flex-row'>
+                        <h2 className='font-semibold'>{concert.date}</h2>&nbsp;{concert.viola !== "" && <h2>{concert.viola}</h2>}&nbsp;{concert.conductor !== "" && <h2>{concert.conductor}</h2>}
+                    </div>
+                <h5>{concert.location}<br/>
+                {concert.programme.map((prog, ind)=>(
+                    <div key={ind}>
+                    <p>♪ {prog}</p>
+                    </div>
+                ))}
+                </h5>
+                </div>
+            ))}
+          </div>
+    </div>
+  )
+}
