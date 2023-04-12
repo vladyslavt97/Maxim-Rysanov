d6cf620a5c1a9507f771d43810b1ca5e3d40d301
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-12 10:58:55 +0200

    Contacts

--- pages/contacts.tsx
+++ pages/contacts.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/Header'
 import Link from 'next/link'
 import React from 'react'
 import { AiOutlinePhone } from 'react-icons/ai';
+import { FiMail } from 'react-icons/fi';
 type Props = {}
 
 export default function Contacts({}: Props) {
@@ -16,14 +17,16 @@ export default function Contacts({}: Props) {
             <div className='p-4 flex justify-center items-center flex-col text-center'>
                 <div className=''>
                     <h1 className='font-semibold'>General Management</h1>
-                    <p>Lorna Neill<br/>
+                    <p><span className='italic'>Lorna Neill</span><br/>
                     <div className='flex flex-row text-center justify-center items-center gap-3'>
                         <AiOutlinePhone/>+44 (0) 20 8051 9476<br/>
                     </div>
                     <div className='flex flex-row text-center justify-center items-center gap-3'>
                         <AiOutlinePhone/>+44 (0) 7798 531 819<br/>
                     </div>
-                    lorna@musicinteralia.com<br/>
+                    <div className='flex flex-row text-center justify-center items-center gap-2'>
+                        <FiMail/>lorna@musicinteralia.com<br/>
+                    </div>
                     <Link href="http://www.musicinteralia.com" className='text-blue-900'>
                     musicinteralia.com<br/>
                     </Link>
@@ -32,8 +35,10 @@ export default function Contacts({}: Props) {
                 <br/>
                 <div>
                     <h1 className='font-semibold'>Benelux</h1>
-                    <p>Marjon Koenekoop<br/>
-                    marjon@ivyartists.com<br/>
+                    <p><span className='italic'>Marjon Koenekoop</span><br/>
+                    <div className='flex flex-row text-center justify-center items-center gap-2'>
+                        <FiMail/>marjon@ivyartists.com<br/>
+                    </div>
                     <Link href="http://www.ivyartists.com" className='text-blue-900'>
                     ivyartists.com<br/>
                     </Link>
@@ -42,7 +47,10 @@ export default function Contacts({}: Props) {
                 <br/>
                 <div>
                     <h1 className='font-semibold'>Italy & various projects</h1>
-                    <p>Susanna Stefani susanna@onylstage.co.uk<br/>
+                    <p><span className='italic'>Susanna Stefani</span><br/> 
+                    <div className='flex flex-row text-center justify-center items-center gap-2'>
+                        <FiMail/>susanna@onylstage.co.uk<br/>
+                    </div>
                     <div className='flex flex-row text-center justify-center items-center gap-3'>
                     <AiOutlinePhone/> +44 7950 427967<br/>
                     </div>
@@ -50,16 +58,18 @@ export default function Contacts({}: Props) {
                     onlystage.co.uk/rysanov<br/>
                     </Link>
                     <Link href="http://www.onlystage.co.uk/rysanov-conductor/" className='text-blue-900'>
-                    onlystage.co.uk/rysanov-conductor/<br/>
+                    onlystage.co.uk/rysanov-conductor<br/>
                     </Link>
                     </p>
                 </div>
                 <br/>
                 <div>
                     <h1 className='font-semibold'>Scandinavia</h1>
-                    <p>Neel Teilmann<br/>
+                    <p><span className='italic'>Neel Teilmann</span><br/>
                     Nordic Artists Management A/S<br/>
-                    net@nordicartistsmanagement.com<br/>
+                    <div className='flex flex-row text-center justify-center items-center gap-2'>
+                        <FiMail/>net@nordicartistsmanagement.com<br/>
+                    </div>
                     <div className='flex flex-row text-center justify-center items-center gap-3'>
                         <AiOutlinePhone/>+45 22 744 904<br/>
                     </div>
@@ -71,9 +81,11 @@ export default function Contacts({}: Props) {
                 <br/>
                 <div>
                     <h1 className='font-semibold'>Spain</h1>
-                    <p>CONCIERTOS AUGUSTO S.L.<br/>
-                    Gonzalo Augusto<br/>
-                    gonzalo@conciertosaugusto.com<br/>
+                    <p><span className='italic'>Gonzalo Augusto</span><br/>
+                    Concertos Augusto S.L.<br/>
+                    <div className='flex flex-row text-center justify-center items-center gap-2'>
+                        <FiMail/>gonzalo@conciertosaugusto.com<br/>
+                    </div>
                     <div className='flex flex-row text-center justify-center items-center gap-3'>
                         <AiOutlinePhone/>+34 91 634 02 05<br/>
                     </div>
