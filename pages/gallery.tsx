8a10707293ce0c9edd63eafa8d73b9c313adb286
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-04-12 10:47:26 +0200

    Gallery finished

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -6,6 +6,7 @@ import mr1 from "../public/gallery/mr1.jpg"
 import mr2 from "../public/gallery/mr2.jpg"
 import mr3 from "../public/gallery/mr3.jpg"
 import mr4 from "../public/gallery/mr4.jpg"
+import mr5 from "../public/gallery/mr5.jpg"
 import { Grid } from  'react-loader-spinner'
 type Props = {}
 const waiting = () =>{
@@ -23,18 +24,18 @@ const waiting = () =>{
 
 export default function Contacts({}: Props) {
   return (
-    <div className='h-[85vh] flex justify-center items-center flex-col text-black bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px]'>
-        <h1 className='font-bold mb-5'>Gallery</h1>
-        <div className='flex flex-row overflow-scroll gap-10 justify-start pl-10 pb-5'>
-            {mr1 ? <Image src={mr1} alt="photo" width={250} height={250} className='rounded-lg'/>: waiting()}
-            <Image src="/gallery/mr2.jpg" alt="photo" width={250} height={250} className='rounded-lg'/>
-            <Image src="/gallery/mr3.jpg" alt="photo" width={250} height={250} className='rounded-lg'/>
-            <Image src="/gallery/mr4.jpg" alt="photo" width={250} height={250} className='rounded-lg'/>
-            <Image src="/gallery/mr5.jpg" alt="photo" width={250} height={250} className='rounded-lg'/>
-        </div>
-        <Link href="https://www.dropbox.com/sh/jcicv8jc17xq1cp/AABx6m5r-R38LxWhj69mtE-ra?dl=0" target="_blank">
-            <button className='border-2 rounded-2xl p-1'>Download Photos Here</button>
+    <div className='h-[85vh] flex justify-start items-center flex-col text-black bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px]'>
+        <h1 className='font-bold m-5'>Gallery</h1>
+        <Link href="https://www.dropbox.com/sh/jcicv8jc17xq1cp/AABx6m5r-R38LxWhj69mtE-ra?dl=0" target="_blank" className='absolute right-0'>
+            <button className='border-2 rounded-2xl p-1 border-gray-300 bg-gradient-to-r from-yellow-100 via-yellow-200 to-yellow-300 text-xs m-1'>Download Photos</button>
         </Link>
+        <div className='flex flex-row overflow-scroll gap-10 justify-start pl-10 pb-5'>
+            <Image src={mr1} alt="photo" width={250} height={250} className='rounded-lg'/>
+            <Image src={mr2} alt="photo" width={250} height={250} className='rounded-lg'/>
+            <Image src={mr3} alt="photo" width={250} height={250} className='rounded-lg'/>
+            <Image src={mr4} alt="photo" width={250} height={250} className='rounded-lg'/>
+            <Image src={mr5} alt="photo" width={250} height={250} className='rounded-lg'/>
+        </div>
         <Footer />
     </div>
   )
