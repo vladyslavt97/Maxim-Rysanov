3a49dfec4157423574b2c3e688c722fae362c1bc
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-05-04 22:46:59 +0200

    Bio updated

--- components/EngBio.tsx
+++ components/EngBio.tsx
@@ -3,15 +3,39 @@ type Props = {}
 export default function EngBio({}: Props) {
   return (
     <div>
-        <p>Grammy nominated Ukrainian-British violist and conductor Maxim Rysanov has established himself as one of the worldʼs most vibrant and charismatic musicians. As a violist, he is principally known as a frequent guest of the crème of the international music scene, such as BBC Last Night of the Proms and the festivals of Edinburgh, Salzburg and Verbier.
+        <p>Grammy nominated Ukrainian-British violist and conductor Maxim Rysanov has established himself as
+            one of the worldʼs most vibrant and charismatic musicians. As a violist, he is principally known as a
+            frequent guest of the crème of the international music scene. Parallel to playing viola, Maxim enjoys a
+            thriving career as a conductor, often combining conducting and directing as a soloist within one concert.
             <br/>
             <br/>
 
-            Among his concerto highlights are the Mariinsky Orchestra (Valery Gergiev), Russian National Orchestra (Mikhail Pletnev), BBC Symphony Orchestra (Jiří Bělohlávek), Residentie Orkest the Hague (Neeme Jarvi), Svetlanov State Symphony Orchestra (Vladimir Jurovsky), Seattle Symphony (Andrey Boreyko), Orquesta Sinfónica de Castilla y León (Vasily Petrenko), Deutsches Symphonie-Orchester Berlin (Tugan Sokhiev), Moscow Philharmonic (Yuri Simonov), Bournemouth Symphony Orchestra (Vassily Sinaisky), Gürzenich Orchestra Cologne (Sir Mark Elder), Frankfurt Radio Symphony Orchestra (Philippe Herreweghe), NDR Philharmonic Orchestra (Eivind Gullberg Jensen), Prague Radio Symphony Orchestra (Juraj Valcuha and again with Michał Nesterowicz), Adelaide Symphony Orchestra (Arvo Volmer), Danish National Symphony Orchestra (Michael Schoenwandt),  Shanghai Symphony (Long Yu), European Union Youth Orchestra (Matthias Bamert), Czech Filharmonic (John Axelrod), Scottish Chamber Orchestra (Olari Elts), Spanish National Orchestra (Josep Caballe Domenech) and Beethovenorchester Bonn  (Dirk  Kaftan).
+            Among his concerto highlights he counts the following: the Mariinsky Orchestra, Russian National
+            Orchestra, BBC Symphony Orchestra, Residentie Orkest the Hague, Svetlanov State Orchestra,
+            Seattle Symphony, Orquesta Sinfónica de Castilla y León, Deutsches Symphonie-Orchester Berlin,
+            Moscow Philharmonic, Bournemouth Symphony Orchestra, Gürzenich Orchestra Cologne, Frankfurt
+            Radio Symphony Orchestra, NDR Philharmonic Orchestra, Prague Radio Symphony Orchestra,
+            Adelaide Symphony Orchestra, West Australian Symphony Orchestra, Danish National Symphony
+            Orchestra, Tampere Philharmonic, Turku Philharmonic, Nordic Chamber Orchestra, Shanghai
+            Symphony, Guangzhou Symphony Orchestra, European Union Youth Orchestra, Scottish Chamber
+            Orchestra, Spanish National Orchestra, Beethoven Orchester Bonn, Hong Kong Sinfonietta, Brno
+            Philharmonic, Real Orquesta Sinfónica Sevilla and Orquesta Filarmónica de Gran Canaria.
             <br/>   
             <br/>
 
-            Maxim enjoys a busy career as a conductor parallel to playing the viola. Often he combines conducting and directing a concerto from viola. Maxim studied conducting with Alan Hazeldine at the Guildhall School of Music and Drama, and has taken part in several masterclasses with Gennady Rozhdestvensky and Jorma Panula. Here are some of the orchestras he conducted: Svetlanov State Symphony Orchestra, La Verdi Orchestra Milan, Basel Symphony Orchestra, Russian National Orchestra, Tatarstan State Symphony Orchestra, Spanish Radio Orchestra (RTVE), Orquesta Sinfónica de Castilla y León, Dala Sinfonietta, Danubia Symphony Budapest, London Mozart Players, Southbank Sinfonia, Riga Sinfonietta, Lithuanian Chamber Orchestra, I Musici  de Montreal, Detmold Chamber Orchestra, Musica Viva (Moscow), Voronezh Youth Orchestra, Kiev Soloists, Częstochowa Philharmonic Orchestra, Chamber Orchestra Chaarts, Budapest Festival Academy Orchestra, Classic FM Orchestra (Bulgaria), Georgian National Symphony Orchestra, Plovdiv Philharmonic, Sofia Opera Orchestra and Chorus, Sofia Philharmonic, Pfortseim Theatre Orchestra, Badische Philharmonie, Vivaldi Orchestra Morbegno. 
+            Conducting highlights of the 2022/23 season will be his returns to La Verdi Milan, I Musici de Montreal
+            and Croatian Radio Symphony Orchestra.
+            <br/>   
+            <br/>
+
+            The past season saw the world premiere of a new viola concerto by Gabriel Prokofiev. The first
+            performances also marked his debut as a conductor with the Beethoven Orchestra Bonn and with the
+            BBC National Orchestra of Wales, both of which have co-commissioned the new work together with La
+            Verdi Orchestra Milan and the Oregon Music Festival. Further conducting highlights were his debuts
+            with Ulster Orchestra, Orchestra of East Netherlands and Croatian Radio Symphony Orchestra, as well
+            as return visits to BBC NOW, Detmold Chamber Orchestra, Kiev Soloists and Danubia Orchestra.
+            Performing debuts with St Gallen Symphony Orchestra, Tenerife Symphony Orchestra, and Pannon
+            Philharmonic rounded off the season.
             <br/> 
             <br/>
 
@@ -19,22 +43,72 @@ export default function EngBio({}: Props) {
             <br/> 
             <br/>
 
-            His enthusiasm for new music generates many exciting collaborations which extend the viola repertoire. This includes world premieres by Dobrinka Tabakova, Pēteris Vasks, Richard Dubugnon, Mate Balogh and Elena Langer. Other composers with whom Maxim has developed a close working relationship include Benjamin Yusupov, Leonid Desyatnikov, Giya Kancheli and most recently Gabriel Prokofiev. Gabriel is planning to write a symphony-concerto for Maxim, where the performer will have to combine skills of both soloist and conductor.
+            Further highlights of the 2022/23 season will be his performing debuts with the Orchestra della Svizzera
+            Italiana, Belgrade Chamber Orchestra and the CSO Presidential Orchestra in Ankara, as well as return
+            visits to the Royal Philharmonic Orchestra under Vasily Petrenko at the Ljubliana Festival, a recording
+            project with the Hallé Orchestra and invitations to Schleswig-Holstein Musik Festival, Carl Nielsen
+            Festival Odense, O Modernt Stockholm, Moritzburg Festival and Westport Festival.
             <br/> 
             <br/>
 
-            Maxim&apos;s recordings have gained numerous award nominations including Gramophone Editor&apos;s Choice, ECHO, ICMA. Maxim featured as both soloist and conductor on the debut disc of composer Dobrinka Tabakova (ECM) in 2013 - a disc that reached number 2 in the UK classical charts and was shortlisted for a Grammy Award. His last album “in Schubert’s company” was selected as Gramophone Critic’s Choice of the Year.   New CD is planned for a spring 2020 release on BIS. Sinfonietta Riga under Maxim’s direction are performing music by Peteris Vasks. It will include the premier recording of the Viola Concerto written and dedicated to Maxim, coupled with the Symphony no 1. 
+            Highlights of previous seasons were Maxim’s debuts with Hamburger Symphoniker under Sylvain
+            Cambreling, Polish Baltic Philharmonic Gdansk and Croatian Radio Symphony Orchestra as well as
+            appearances at Madrid’s Auditorio Nacional, Konzerthaus Vienna, Tchaikovsky Hall Moscow, Marinsky
+            Theatre St Petersburg, Teatro Liceo Salamanca and the festivals in Avila, Budapest and
+            Frederiskvaerk.
             <br/> 
             <br/>
 
-            Maxim is a recipient of various awards, including the Gramophone Young Artist of the Year Award and the BBC Radio 3 New Generation Award. He is also a prize-winner at the Geneva, Lionel Tertis and Valentino Bucchi competitions. 
+            Maxim studied conducting with Alan Hazeldine at the Guildhall School of Music and Drama, and has
+            taken part in several masterclasses with Gennady Rozhdestvensky, Jorma Panula and Oleg Caetani.
+            Orchestras where he has appeared as a conductor include: Svetlanov State Symphony Orchestra, La
+            Verdi Orchestra Milan, Basel Symphony Orchestra, Russian National Orchestra, Tatarstan State
+            Symphony Orchestra, Spanish Radio Orchestra (RTVE), Orquesta Sinfónica de Castilla y León, Dala
+            Sinfonietta, Danubia Symphony Budapest, London Mozart Players, Southbank Sinfonia, Riga
+            Sinfonietta, Lithuanian Chamber Orchestra, I Musici de Montreal, Detmold Chamber Orchestra,
+            Musica Viva (Moscow), Voronezh Youth Orchestra, Kiev Soloists, Częstochowa Philharmonic
+            Orchestra, Chamber Orchestra Chaarts, Budapest Festival Academy Orchestra, Classic FM Orchestra
+            (Bulgaria), Georgian National Symphony Orchestra, Plovdiv Philharmonic, Sofia Opera Orchestra and
+            Chorus, Sofia Philharmonic, Pforzheim Theatre Orchestra, Badische Philharmonie and Vivaldi
+            Orchestra Morbegno.
             <br/> 
             <br/>
 
-            Maxim studied viola with Maria Sitkovskaya in Moscow and John Glickman at the Guildhall School of Music and Drama London. 
+            Maxim is a keen chamber musician. His chamber partners include Maxim Vengerov, Janine Jansen,
+            Mischa Maisky, Gidon Kremer, Nicola Benedetti, Vadim Repin, Augustin Dumay, Viktoria Mullova,
+            Alexander Sitkovetsky, Sol Gabetta, Leif Ove Andsnes, Denis Matsuev, Alessio Bax, Michael Collins,
+            Martin Frost, Alice Coote, Freddy Kempf and Yeol Eum Son among others.
             <br/>
             <br/> 
-            He is delighted to play a 1780 Giuseppe Guadagnini Viola known as &apos;II Soldato&apos;, on private loan that was kindly arranged by Premiere Performances of Hong Kong.
+
+            His enthusiasm for new music generates many exciting collaborations which extend the viola repertoire.
+            This includes world premieres by Dobrinka Tabakova, Pēteris Vasks, Richard Dubugnon and Valentin
+            Bibik. Other composers with whom Maxim has developed a close working relationship include Benjamin
+            Yusupov, Leonid Desyatnikov, Giya Kancheli, Artem Vassiliev and Elena Langer and most recently
+            Gabriel Prokofiev.
+            <br/> 
+            <br/>
+
+            Maxim&apos;s recordings have gained numerous award nominations including Gramophone Editor&apos;s Choice,
+            ECHO and ICMA. Maxim featured as both soloist and conductor on the debut disc of composer
+            Dobrinka Tabakova (ECM) in 2013 - a disc that reached number 2 in the UK classical charts and was
+            shortlisted for a Grammy Award. His album “In Schubert’s company” was selected as Gramophone
+            Critic’s Choice of the Year. The latest disc “Voices” on the BIS label with Riga Sinfonietta features music
+            of Pēteris Vasks, including the viola concerto which was written for and dedicated to Maxim, as well as
+            the Symphony No. 1 which Maxim conducts. This was released in spring 2020 to great critical acclaim.
+            Maxim is a recipient of various awards, including the Gramophone Young Artist of the Year Award and
+            the BBC Radio 3 New Generation Award. He is also a prize-winner at the Geneva, Lionel Tertis and
+            Valentino Bucchi competitions.
+            <br/> 
+            <br/>
+
+            Maxim studied viola with Maria Sitkovskaya in Moscow and John Glickman at the Guildhall School of
+            Music and Drama London.
+            <br/> 
+            <br/>
+
+            He is delighted to play a 1780 Giuseppe Guadagnini viola known as ‘Il Soldato’ on private loan that was
+            kindly arranged by Premiere Performances Hong Kong.
             </p>
     </div>
   )
--- pages/bio.tsx
+++ pages/bio.tsx
@@ -6,6 +6,7 @@ import LanguagesSwitch from '@/components/LanguagesSwitch'
 import EngBio from '@/components/EngBio';
 import UkrBio from '@/components/UkrBio';
 import RusBio from '@/components/RusBio';
+import { HiDownload } from 'react-icons/hi';
 
 type Props = {}
 
@@ -21,6 +22,7 @@ export default function Bio({}: Props) {
     <div className='h-full bg-gray-400'>
         <div className='absolute top-[82px] flex flex-col justify-center items-center mx-2 p-5 pb-40 md:pb-40 text-black bg-gradient-to-tr from-neutral-100 to-gray-200 rounded md:p-10 lg:px-20'>
             {/* <LanguagesSwitch/> */}
+            <a href="/Rysanov_Biography_22-23.pdf" download className='absolute top-5 right-0'><HiDownload className='text-xl mx-2'/></a>
             <div className='flex justify-center items-center'>
               <h1 className='font-bold text-xl m-5 text-black'>Biography</h1>
             </div>
