2c439dd9b551230fd2f9bce6c9ab4c4eafbd0eb6
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-08-27 11:03:04 +0200

    dropbox link updated

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -14,7 +14,7 @@ export default function Contacts({}: Props) {
   return (
     <div className='h-[85vh] flex justify-start md:justify-evenly items-center flex-col text-black bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px]'>
         <h1 className='font-bold m-5'>Gallery</h1>
-        <Link href="https://www.dropbox.com/sh/jcicv8jc17xq1cp/AABx6m5r-R38LxWhj69mtE-ra?dl=0" target="_blank" className='absolute right-0 top-0'>
+        <Link href="https://www.dropbox.com/sh/n85yubpfsm6oum1/AACEfvpj7mviGZsAyod1svHwa?dl=0" target="_blank" className='absolute right-0 top-0'>
             <button className='border-2 rounded-2xl p-1 border-gray-300 bg-gradient-to-r from-yellow-100 via-yellow-200 to-yellow-300 text-xs m-1'>Download Photos</button>
         </Link>
         <div className='flex flex-row overflow-x-scroll gap-1 justify-start pl-1 pb-5 mx-20 overflow-y-hidden scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#303030]/80'>
