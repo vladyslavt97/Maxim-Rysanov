942b615ffe7daec452e36452fcdecd75a296019b
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2023-11-07 15:19:50 +0100

    flex-row wrap on reviews

--- pages/reviews.tsx
+++ pages/reviews.tsx
@@ -7,9 +7,9 @@ export default function Reviews({}: Props) {
     <div className=" text-yellow-700 mb-24">
         <h1 className="mt-24 text-center text-lg text-black font-bold m-5">Reviews</h1>
 
-        <div className="flex flex-col gap-5 items-stretch mx-5 w-100">
+        <div className="flex flex-col md:flex-row md:flex-wrap gap-5 items-stretch mx-5 w-100">
             {reviews.map((review, i)=>(
-                <div key={i} className="rounded-lg border-2 border-gray-500/10 shadow-lg px-2 py-1 bg-white">
+                <div key={i} className="rounded-lg border-2 border-gray-500/10 shadow-lg px-2 py-1 bg-white md:w-[350px] md:m-auto">
                     <h1>{review.CDTitle}</h1>
                     <h2 className="leading-8">&ldquo;{review?.quote}&ldquo;</h2>
                     <span className="text-gray-800">{review.link ? <a href={review.link}>{review.byWhom}</a> : <h2>{review.byWhom}</h2>}</span>
