1a59b99f0c77ff0b312db534a4e42c55cc56ecb8
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2024-03-08 23:17:26 +0100

    2023 concerts page

--- pages/concerts-2023.tsx
+++ pages/concerts-2023.tsx
@@ -11,7 +11,7 @@ export default function Concerts2023({}: Props) {
         <div className="bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px] rounded text-black min-h-full flex flex-col items-center">
             <PastConcerts />
             <div className="absolute left-6 md:static md:flex md:justify-center md:items-center pt-5">
-                <h1 className="font-bold text-xl">2022</h1>
+                <h1 className="font-bold text-xl">2023</h1>
             </div>
             <div className="flex flex-col mx-3 mt-16 justify-center items-start mb-40 lg:text-xl">
                 {validConcerts2023.map(
