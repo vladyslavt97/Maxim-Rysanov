5792e2d591cdcac58d77438a065723bb3a53d86e
vladyslavt97 <vladyslavt97@gmail.com>
2024-04-27 11:33:15 +0200

    gallery updated

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,13 +1,6 @@
-import Image from 'next/image'
+import Gallery from '@/components/Gallery'
 import Link from 'next/link'
-import mr1 from "../public/gallery/mr1.jpg"
-import mr2 from "../public/gallery/mr2.jpg"
-import mr3 from "../public/gallery/mr3.jpg"
-import mr4 from "../public/gallery/mr4.jpg"
-import mr5 from "../public/gallery/mr5.jpg"
-import mr6 from "../public/gallery/mr6.jpg"
-import mr7 from "../public/gallery/mr7.jpg"
-import mr8 from "../public/gallery/mr8.jpg"
+
 type Props = {}
 
 export default function Contacts({}: Props) {
@@ -17,16 +10,7 @@ export default function Contacts({}: Props) {
         <Link href="https://www.dropbox.com/sh/n85yubpfsm6oum1/AACEfvpj7mviGZsAyod1svHwa?dl=0" target="_blank" className='absolute right-0 top-0'>
             <button className='border-2 rounded-2xl p-1 border-gray-300 bg-gradient-to-r from-yellow-100 via-yellow-200 to-yellow-300 text-xs m-1'>Download Photos</button>
         </Link>
-        <div className='flex flex-row overflow-x-scroll gap-1 justify-start pl-1 pb-5 mx-20 overflow-y-hidden scrollbar'>
-            <Image src={mr1} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr2} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr3} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr6} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr4} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr7} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr5} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-            <Image src={mr8} alt="photo" width={250} height={250} className='rounded-lg object-contain'/>
-        </div>
+        <Gallery/>
     </div>
   )
 }
