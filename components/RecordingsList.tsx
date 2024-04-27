443b65a544451d37eb6ad10994299364ff1a0e89
vladyslavt97 <vladyslavt97@gmail.com>
2024-04-27 11:10:28 +0200

    recs text black

--- components/RecordingsList.tsx
+++ components/RecordingsList.tsx
@@ -48,7 +48,7 @@ export default function RecordingsList({}: Props) {
                     </Link>
                     <div className="flex flex-col italic hover:font-semibold hover:text-gray-500">
                         <Link href={r.href} className="flex flex-row mt-1">
-                            <h1 className="text-sm px-1">
+                            <h1 className="text-sm px-1 text-black">
                                 Listen here:
                             </h1>
                             <Image
