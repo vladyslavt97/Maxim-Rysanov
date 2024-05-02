36552f85432a32224a16e6a15b6c83f382f3a36d
vladyslavt97 <vladyslavt97@gmail.com>
2024-05-02 17:09:41 +0200

    new component

---
+++ components/HorizontalGallery.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function HorizontalGallery({}: Props) {
+    const images = ["/gallery/mr1.jpg", "/gallery/mr2.jpg", "/gallery/mr3.jpg","/gallery/mr4.jpg", "/gallery/mr5.jpg", "/gallery/mr6.jpg","/gallery/mr7.jpg", "/gallery/mr8.jpg", "/gallery/mr9.jpg"];
+
+  return (
+    <div></div>
+  )
+}
--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,4 +1,5 @@
 import Gallery from '@/components/Gallery'
+import HorizontalGallery from '@/components/HorizontalGallery'
 import Link from 'next/link'
 
 type Props = {}
@@ -10,7 +11,8 @@ export default function Contacts({}: Props) {
         <Link href="https://www.dropbox.com/sh/n85yubpfsm6oum1/AACEfvpj7mviGZsAyod1svHwa?dl=0" target="_blank" className='absolute right-0 top-0'>
             <button className='border-2 rounded-2xl p-1 border-gray-300 bg-gradient-to-r from-yellow-100 via-yellow-200 to-yellow-300 text-xs m-1'>Download Photos</button>
         </Link>
-        <Gallery/>
+        {/* <Gallery/> */}
+        <HorizontalGallery/>
     </div>
   )
 }
