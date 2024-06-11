27455c18a4291f53fa3f92acfaa263a0f06cf362
vladyslavt97 <vladyslavt97@gmail.com>
2024-06-11 08:16:50 +0200

    recordings

--- components/RecordingsList.tsx
+++ components/RecordingsList.tsx
@@ -23,206 +23,224 @@ export default function RecordingsList({}: Props) {
     }
   };
   return (
-    <div className="flex flex-row flex-wrap justify-center py-24 bg-gray-300">
-      <h1 className="text-center font-bold text-2xl text-gray-500">
+    <div className="flex justify-center pt-24 bg-gray-300">
+      <h1 className="text-center absolute font-bold text-2xl text-gray-500">
         Recordings
       </h1>
-      {recordings.map((r) => (
-        <div
-          key={r.imageSrc}
-          className="hover:scale-110 transition-transform duration-2000"
-        >
-          <motion.div
-            initial={{ opacity: 0, scale: 0.9 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 1 }}
-            viewport={{ once: true }}
-            className="mb-10 mt-5 mx-10 shadow-xl rounded-2xl bg-gray-300 relative"
+      <div className="mt-5 flex flex-row flex-wrap justify-center py-5">
+        {recordings.map((r) => (
+          <div
+            key={r.imageSrc}
+            className="hover:scale-110 transition-transform duration-2000"
           >
-            {r.newrelease && (
-              <div className=" bg-blue-400 -skew-x-12 transform absolute -top-4 -left-4 text-white px-1 rounded">
-                New Release
+            <motion.div
+              initial={{ opacity: 0, scale: 0.9 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{ duration: 1 }}
+              viewport={{ once: true }}
+              className="mb-10 mt-5 mx-10 shadow-xl rounded-2xl bg-gray-300 relative"
+            >
+              {r.newrelease && (
+                <div className=" bg-blue-400 -skew-x-12 transform absolute -top-4 -left-4 text-white px-1 rounded">
+                  New Release
+                </div>
+              )}
+              <Image
+                src={r.imageSrc}
+                alt={r.imageSrc}
+                width={1000}
+                height={1000}
+                className="rounded-2xl w-[300px] h-[300px]"
+                loading="eager"
+                priority={true}
+              />
+              <div className="flex flex-col italic hover:font-semibold hover:text-gray-500">
+                <div className="flex flex-row items-center">
+                  {(r.applemusic || r.spotify) && (
+                    <h1 className="text-sm px-1 text-black">Stream here:</h1>
+                  )}
+                  {r.applemusic && (
+                    <Link
+                      href={r.applemusic}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/applemusic.png"
+                        alt="logo"
+                        width={50}
+                        height={50}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.spotify && (
+                    <Link
+                      href={r.spotify}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/spotify.png"
+                        alt="logo"
+                        width={50}
+                        height={50}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.hyperion && (
+                    <Link
+                      href={r.hyperion}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/hyperion.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.bis && (
+                    <Link
+                      href={r.bis}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/BIS.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.onyx && (
+                    <Link
+                      href={r.onyx}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/ONYX.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.avie && (
+                    <Link
+                      href={r.avie}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/AVIE.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.decca && (
+                    <Link
+                      href={r.decca}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/DECCA.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.ecm && (
+                    <Link
+                      href={r.ecm}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/ECM.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.warner && (
+                    <Link
+                      href={r.warner}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/warner.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.orchid && (
+                    <Link
+                      href={r.orchid}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/orchid.png"
+                        alt="logo"
+                        width={70}
+                        height={70}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                  {r.hungaroton && (
+                    <Link
+                      href={r.hungaroton}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <Image
+                        src="/hungaroton.png"
+                        alt="logo"
+                        width={100}
+                        height={100}
+                        className="pl-5 object-contain py-2"
+                        loading="eager"
+                      />
+                    </Link>
+                  )}
+                </div>
               </div>
-            )}
-            <Image
-              src={r.imageSrc}
-              alt={r.imageSrc}
-              width={1000}
-              height={1000}
-              className="rounded-2xl w-[300px] h-[300px]"
-              loading="eager"
-              priority={true}
-            />
-            <div className="flex flex-col italic hover:font-semibold hover:text-gray-500">
-              <div className="flex flex-row items-center">
-                {(r.applemusic || r.spotify) && (
-                  <h1 className="text-sm px-1 text-black">Stream here:</h1>
-                )}
-                {r.applemusic && (
-                  <Link
-                    href={r.applemusic}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/applemusic.png"
-                      alt="logo"
-                      width={50}
-                      height={50}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.spotify && (
-                  <Link
-                    href={r.spotify}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/spotify.png"
-                      alt="logo"
-                      width={50}
-                      height={50}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.hyperion && (
-                  <Link
-                    href={r.hyperion}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/hyperion.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.bis && (
-                  <Link href={r.bis} target="_blank" rel="noopener noreferrer">
-                    <Image
-                      src="/BIS.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.onyx && (
-                  <Link href={r.onyx} target="_blank" rel="noopener noreferrer">
-                    <Image
-                      src="/ONYX.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.avie && (
-                  <Link href={r.avie} target="_blank" rel="noopener noreferrer">
-                    <Image
-                      src="/AVIE.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.decca && (
-                  <Link
-                    href={r.decca}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/DECCA.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.ecm && (
-                  <Link href={r.ecm} target="_blank" rel="noopener noreferrer">
-                    <Image
-                      src="/ECM.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.warner && (
-                  <Link
-                    href={r.warner}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/warner.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.orchid && (
-                  <Link
-                    href={r.orchid}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/orchid.png"
-                      alt="logo"
-                      width={70}
-                      height={70}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-                {r.hungaroton && (
-                  <Link
-                    href={r.hungaroton}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    <Image
-                      src="/hungaroton.png"
-                      alt="logo"
-                      width={100}
-                      height={100}
-                      className="pl-5 object-contain py-2"
-                      loading="eager"
-                    />
-                  </Link>
-                )}
-              </div>
-            </div>
-          </motion.div>
-        </div>
-      ))}
+            </motion.div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
