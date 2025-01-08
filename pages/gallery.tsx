61ecf299a5dc6cc99d372135ba6a61ddb1c4be60
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2025-01-08 21:43:28 +0100

    new gallery

--- pages/gallery.tsx
+++ pages/gallery.tsx
@@ -1,3 +1,4 @@
+import { AnimatedTestimonials } from "@/components/AnimatedTestimonials";
 import Gallery from "@/components/Gallery";
 import HorizontalGallery from "@/components/HorizontalGallery";
 import Link from "next/link";
@@ -5,8 +6,27 @@ import Link from "next/link";
 type Props = {};
 
 export default function Contacts({}: Props) {
+    const images = [
+        "/gallery/mr1.jpg",
+        "/gallery/mr2.jpg",
+        "/gallery/mr3.jpg",
+        "/gallery/mr4.jpg",
+        "/gallery/mr5.jpg",
+        "/gallery/mr6.jpg",
+        "/gallery/mr7.jpg",
+        "/gallery/mr8.jpg",
+        "/gallery/mr9.jpg",
+    ];
+
+    const testimonials = images.map((src, index) => ({
+        // quote: "I play with my heart",
+        // name: `Image ${index + 1}`,
+        // designation: "kamermusikverein",
+        src: src,
+    }));
+
     return (
-        <div className="h-[85vh] flex justify-start md:justify-evenly flex-col text-black bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px] ">
+        <div className="h-[85vh] flex justify-start md:justify-evenly flex-col text-black bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px] px-10 md:px-0">
             <h1 className="font-bold absolute top-[20px] left-1/2 transform -translate-x-1/2">
                 Gallery
             </h1>
@@ -20,7 +40,8 @@ export default function Contacts({}: Props) {
                 </button>
             </Link>
             {/* <Gallery/> */}
-            <HorizontalGallery />
+            {/* <HorizontalGallery /> */}
+            <AnimatedTestimonials testimonials={testimonials} />
         </div>
     );
 }
