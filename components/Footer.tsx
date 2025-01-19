f7ebb2e3402dbbe78c59018ecbf04e63619ae460
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2025-01-19 10:46:03 +0100

    footer improved

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
+
 type Props = {};
 
 export default function Footer({}: Props) {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const togglePopup = () => {
+        setShowPopup((prev) => !prev);
+    };
+
     return (
         <div className="fixed bottom-0 w-full bg-gray-800 text-white flex justify-evenly items-center border-t-[10px] border-r-[10px] border-l-[10px] border-gray-300 h-[51px]">
-            {/* <SocialIcon
-                url="https://www.facebook.com/maximrysanov"
-                network="facebook"
-                style={{ height: 35, width: 35 }}
-                className="m-1 cursor-pointer"
-            />
-            <SocialIcon
-                url="https://www.youtube.com/@MaximRysanov"
-                network="youtube"
-                style={{ height: 35, width: 35 }}
-                className="m-1 cursor-pointer"
-            />
-            <SocialIcon
-                url=""
-                network="instagram"
-                style={{ height: 35, width: 35 }}
-                className="m-1 cursor-pointer"
-            /> */}
             <Link href="https://www.instagram.com/mrysanov_viola_conductor">
                 <Image
                     src="/social/instagram.png"
@@ -32,15 +22,38 @@ export default function Footer({}: Props) {
                     priority={true}
                 />
             </Link>
-            <Link href="https://www.youtube.com/@MaximRysanov">
-                <Image
-                    src="/social/youtube.png"
-                    alt="logo"
-                    width={30}
-                    height={20}
-                    priority={true}
-                />
-            </Link>
+            <div className="relative">
+                <button onClick={togglePopup}>
+                    <Image
+                        src="/social/youtube.png"
+                        alt="logo"
+                        width={30}
+                        height={20}
+                        priority={true}
+                    />
+                </button>
+                {showPopup && (
+                    <div className="absolute bottom-[40px] left-[-60px] bg-gray-300 text-black px-5 py-0 rounded-md shadow-md z-50 flex items-center gap-3">
+                        <Link
+                            href="https://www.youtube.com/watch?v=oK1XGDphjBg&list=PL_YslfPtQb_ZVYSBfjVVoGg3VJpyHyCb8"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:underline"
+                        >
+                            Conducting
+                        </Link>
+                        <div className="h-8 w-[1px] bg-gray-500"></div>
+                        <Link
+                            href="https://www.youtube.com/@MaximRysanov"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="hover:underline"
+                        >
+                            Channel
+                        </Link>
+                    </div>
+                )}
+            </div>
             <Link href="https://www.facebook.com/maximrysanov">
                 <Image
                     src="/social/facebook.png"
