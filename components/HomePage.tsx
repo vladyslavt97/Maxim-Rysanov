8cff7659a3ce5fa37720d5b0c85e46383cb3d9ca
Tsurkanenko Vladyslav <VTsurkanenko@stroeer.de>
2025-04-28 22:40:17 +0200

    scrollabel reviews

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -16,12 +16,39 @@ export default function HomePage({}: Props) {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 transition={{ duration: 2, delay: 1.5 }}
-                className="absolute top-[12%] flex justify-center items-start h-[60vh] text-gray-800 flex-col text-sm md:text-md md:text-2xl xl:text-3xl xl:font-normal lg:leading-relaxed md:ml-[5%] lg:ml-[12%]"
+                className="absolute top-[12%] flex justify-start items-start h-[60vh] text-gray-800 flex-col text-sm md:text-md md:text-2xl xl:text-3xl xl:font-normal lg:leading-relaxed md:ml-[5%] lg:ml-[12%] overflow-y-scroll w-[50%]"
             >
-                <div className="italic w-[50%] sm:w-[50%] px-5 pb-1 font-normal md:leading-loose">
+                <div className="italic px-5 pb-1 font-normal md:leading-loose">
                     <p className="text-small">
-                        &quot;The colours and contrasts that Rysanov manages to
-                        extract from the orchestra are truly fabulous!&quot;
+                        He conducted so fantastically that I realized we were dealing with an exceptionally great conductor, someone truly worthy of our admiration — and that’s remarkable considering he’s also one of the greatest living violists.<br /> So, a wonderful instrumental musician who also conducts magnificently.
+                        <br />
+                        <br />
+                        <br />
+                    </p>
+                </div>
+                <div className="italic px-5 pb-1 font-normal md:leading-loose">
+                    <p className="text-small">
+                        Now, maybe I should also explain — not just praise — why Rysanov’s conducting is so outstanding.
+
+                        What makes Rysanov such a brilliant conductor?
+                        I think it’s the fusion of two elements in his work:
+                        First, he possesses an exceptionally precise, exact, to-the-point, musician-supporting manual technique.
+                        There are no wasted gestures, no decorative movements — everything is geared toward bringing the maximum out of the instruments and the ensemble.<br />
+
+                        Secondly, alongside this extremely professional, perhaps even dry technical precision, there’s a wonderfully expressive, passionate, and compelling musicality.<br />
+                        And crucially, these two elements do not cancel each other out.<br />
+
+                        Often we see conductors who are technically excellent but emotionally cold, or those who are passionate but lack clarity and control.<br />
+                        With Rysanov, neither flaw appears: instead, we see a fully-rounded, superb musical personality.<br />
+                        <br />
+                        <br />
+                        <br />
+                    </p>
+                </div>
+                <div className="italic px-5 pb-1 font-normal md:leading-loose">
+                    <p className="text-small">
+                        The colours and contrasts that Rysanov manages to
+                        extract from the orchestra are truly fabulous!
                         <br />
                     </p>
                 </div>
@@ -43,11 +70,11 @@ export default function HomePage({}: Props) {
                     <span className="text-smaller">Crescendo Magazine</span>
                 </motion.p>
                 <br />
-                <div className="italic w-[50%] sm:w-[50%] px-5 py-1 font-normal md:leading-loose">
+                <div className="italic px-5 py-1 font-normal md:leading-loose">
                     <p className="text-small">
-                        &quot;It’s wonderful to find a recording like this; one
+                        It’s wonderful to find a recording like this; one
                         that, as soon as it finishes, you want to start it all
-                        over again.&quot;
+                        over again.
                     </p>
                 </div>
                 <Link href="/recordings" className="relative inline-block mx-5">
