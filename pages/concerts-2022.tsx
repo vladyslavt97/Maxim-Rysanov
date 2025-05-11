42c50e84823b083ace35b5c3540ba4a5f244ac76
Tsurkanenko Vladyslav <VTsurkanenko@stroeer.de>
2025-05-11 21:11:04 +0200

    correcting pages

--- pages/concerts-2022.tsx
+++ pages/concerts-2022.tsx
@@ -8,7 +8,7 @@ type Props = {};
 export default function Concerts2022({}: Props) {
     const validConcerts2022 = sortedConcerts(concerts2022);
     return (
-        <div className="bg-gradient-to-tr from-neutral-100 to-gray-200 w-full border-[10px] border-gray-300 absolute top-[70px] rounded text-black min-h-full flex flex-col items-center">
+        <div className="relative text-black flex flex-col items-center overflow-y-auto h-full">
             <PastConcerts />
             <div className="absolute left-6 md:static md:flex md:justify-center md:items-center pt-5">
                 <h1 className="font-bold text-xl">2022</h1>
