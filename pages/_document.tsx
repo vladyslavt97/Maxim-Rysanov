b81724b29f78627c4fec5ae8c62b65131e03bfc6
Vladyslav Tsurkanenko <vladyslavt97@gmail.com>
2026-01-19 22:03:06 +0100

    its a perfect website now

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head />
-            <body className="bg-white">
+            <body className="bg-[#1f2e41]">
                 <Main />
                 <NextScript />
             </body>
